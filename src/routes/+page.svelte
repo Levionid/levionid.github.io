50a591b8a07eca8e476203356d9182e583a66641
David Vedenichev <davidvedenichev1@gmail.com>
2025-07-10 02:52:10 +0500

    New meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,7 @@
 	<meta property="og:title" content="Lestix - Главная" />
 	<meta property="og:description" content="Сервер с крутейшим сообществом и кучей эмоций!" />
 	<meta property="og:url" content="https://levionid.github.io/" />
-	<meta property="og:image" content="https://levionid.github.io/meta-image.jpeg" />
+	<meta property="og:image" content="https://levionid.github.io/meta-image.png" />
 </svelte:head>
 
 <div class="page-wrapper">
--- src/routes/important/+page.svelte
+++ src/routes/important/+page.svelte
@@ -10,7 +10,7 @@
 		content="Решение проблемы двухнедельной фазы и сохранение интереса к игре."
 	/>
 	<meta property="og:url" content="https://levionid.github.io/important" />
-	<meta property="og:image" content="https://levionid.github.io/meta-image.jpeg" />
+	<meta property="og:image" content="https://levionid.github.io/meta-image.png" />
 </svelte:head>
 
 <header class="hero hero-small">
