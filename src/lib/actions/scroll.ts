27b5acfcbcff48fc2cf04b3d8b97953220c9f3fb
David Vedenichev <davidvedenichev1@gmail.com>
2025-07-14 22:42:19 +0500

    Some fix

--- src/lib/actions/scroll.ts
+++ src/lib/actions/scroll.ts
@@ -1,17 +1,9 @@
-/**
- * Плавно прокручивает страницу к элементу, соответствующему CSS-селектору.
- * @param selector CSS-селектор целевого элемента (например, '#my-id').
- */
 export function scrollToTarget(selector: string) {
-	// Проверяем, передан ли селектор
 	if (!selector) return;
 	
-	// Находим целевой элемент в DOM
 	const element = document.querySelector(selector);
 	if (!element) return;
 
-	// Определяем отступ в зависимости от ширины экрана,
-	// чтобы заголовок не перекрывал контент
 	let offset;
 	const screenWidth = window.innerWidth;
 	if (screenWidth < 600) {
@@ -20,11 +12,9 @@ export function scrollToTarget(selector: string) {
 		offset = 110; // Отступ для десктопов
 	}
 
-	// Рассчитываем итоговую позицию для прокрутки
 	const elementPosition = element.getBoundingClientRect().top;
 	const offsetPosition = elementPosition + window.pageYOffset - offset;
 
-	// Выполняем плавную прокрутку
 	window.scrollTo({
 		top: offsetPosition,
 		behavior: 'smooth'
